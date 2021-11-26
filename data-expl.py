831d75b9c35b6022a99ee9aa2d20635624d3b0ac
anhhovn <hdma.vn@gmail.com>
2021-11-25 23:33:30 -0700

    updated

--- data-expl.py
+++ data-expl.py
@@ -5,3 +5,13 @@ Created on Thu Nov 25 23:19:24 2021
 @author: hodam
 """
 
+import pandas as pd
+#import file path
+melbourne_file_path = '../data-exploration/melb_data.csv'
+#read file to a variable for easier access
+melbourne_data = pd.read_csv(melbourne_file_path)
+#drop value-missing row
+melbourne_data.dropna(axis=0)
+#describe table and export the result to csv 
+#print(melbourne_data.describe())
+melbourne_data.describe().to_csv('../data-exploration/melb_des.csv')
