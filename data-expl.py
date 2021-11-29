c6ebc85a90e28cbbb34048d2962e707a5ab3c336
anhhovn <hdma.vn@gmail.com>
2021-11-28 21:24:24 -0700

    prediction added

--- data-expl.py
+++ data-expl.py
@@ -22,3 +22,12 @@ y = melbourne_data.Price
 #features
 melbournes_features = ['Rooms', 'Bathroom','Landsize','Lattitude', 'Longtitude']
 X = melbourne_data[melbournes_features]
+# Define model. Specify a number for random_state to ensure same results each run
+melbourne_model = DecisionTreeRegressor(random_state = 1)
+
+#Fit model
+melbourne_model.fit(X, y)
+print("Making predictions for the following 5 houses: ")
+print(X.head())
+print("The predictions are: ")
+print(melbourne_model.predict(X.head()))
